4d1cbed8535c585954f05b4c2bdf0dfaf6af5f68
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2024-06-15 08:13:19 +0600

    Seeding Migration

--- Data/GameStoreContext.cs
+++ Data/GameStoreContext.cs
@@ -7,4 +7,17 @@ public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbCo
 {
     public DbSet<Game> Games => Set<Game>();
     public DbSet<Genre> Genres => Set<Genre>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        Genre[] genres =
+        [
+            new Genre { Id = 1, Name = "Fighting" },
+            new Genre { Id = 2, Name = "RolePlaying" },
+            new Genre { Id = 3, Name = "Sports" },
+            new Genre { Id = 4, Name = "Racing" },
+            new Genre { Id = 5, Name = "Kids and Family" }
+        ];
+        modelBuilder.Entity<Genre>().HasData(genres);
+    }
 }
